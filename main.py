432b666dab3fedd2b53eb08d5a59ca1de595746f
Sumayahx <sumayashittu@gmail.com>
2025-02-01 13:47:48 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from datetime import datetime
+import pytz
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+@app.get("/")
+def get_info():
+    return {
+        "email": "sumayashittu@gmail.com",
+        "current_datetime": datetime.now(pytz.utc).isoformat(),
+        "github_url": "https://github.com/Sumayahx/hng_backend_001"
+    }
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"]
+)
